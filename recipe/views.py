391dc9ffcc46a5487f5008d80d2d3a75541ec6c4
MrSinaMaleki <sina67631@gmail.com>
2024-09-09 12:50:27 +0330

    back-end APIs (Read-Create)

--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from recipe.models import Recipe
+from recipe.serializer import RecipeSerializer
+
+
 # Create your views here.
 
 @api_view(['GET'])
 def recipe_list(request):
-    pass
+    post = Recipe.objects.all()
+    serializer = RecipeSerializer(post, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+@api_view(['POST'])
+def recipe_create(request):
+    serializer = RecipeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
