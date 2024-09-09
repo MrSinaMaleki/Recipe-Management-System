b900e377d0b95b91b7bab9ffeb20907a661078d1
MrSinaMaleki <sina67631@gmail.com>
2024-09-09 12:42:32 +0330

    Recipe Models

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    picture = models.ImageField(upload_to='recipes/')
+
+    def __str__(self):
+        return self.title
