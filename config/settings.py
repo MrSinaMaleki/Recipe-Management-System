8fffbc1e62f55cb80c5eff75499fcb96fa948f37
MrSinaMaleki <sina67631@gmail.com>
2024-09-09 12:46:06 +0330

    serializer.py

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "recipe.apps.RecipeConfig"
+
+    'rest_framework',
+
+    "recipe.apps.RecipeConfig",
 ]
 
 MIDDLEWARE = [
